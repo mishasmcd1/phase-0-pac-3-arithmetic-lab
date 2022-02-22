ece6fb6cde77707887b7c3bd41a245ff39e86469
mishasmcd1 <msm1@uchicago.edu>
2022-02-22 12:23:42 -0600

    commit tree 00669fc8f49885b8b0ba6c1515355013fa051dc7

---
+++ index.js
@@ -0,0 +1,51 @@
+function add() {
+    console.log(add);
+}
+function subtract() {
+    console.log(subtract);
+}
+function multiply() {
+    console.log(multiply);
+}
+function divide() { 
+    console.log(divide);
+}
+function add(a, b) {
+    return (a += b);
+}
+function subtract(a, b) {
+    return (a -= b);
+}
+function multiply(a, b) {
+    return (a *= b);
+}
+function divide(a, b) {
+    return (a /= b);
+}
+function increment(n) {
+    return (n += 1);
+}
+function decrement(n) {
+    return (n -= 1);
+}
+function parseInt() {
+    console.log(parseInt);
+}
+function parseInt(n) {
+        return (n, 10);
+}
+function makeInt() {
+    console.log(makeInt);
+}
+function makeInt(n) {
+    return (parseInt("n"));
+}
+function parseFloat() {
+    console.log(parseFloat);
+}
+function preserveDecimal() {
+    console.log(preserveDecimal);
+}
+function preserveDecimal(n) {
+    return (parseFloat("n"));
+}
